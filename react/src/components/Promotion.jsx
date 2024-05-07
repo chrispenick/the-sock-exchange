e28edb2ebaee0b985b07c14fccbf4a9ed5351698
Chris Penick <chris.penick@webagesolutions.com>
2024-05-07 15:33:28 -0400

    Added Promotion div

---
+++ react/src/components/Promotion.jsx
@@ -0,0 +1,14 @@
+const Promotion = (props) => {
+  return (
+    <div className="card">
+      <div className="card bg-light">
+        <div className="card-text">{props.data.feature}</div>
+        <div className="card-text">
+          <a href="#">Click to buy!</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Promotion;
